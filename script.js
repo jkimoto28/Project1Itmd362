6d9aec48635c28fbc0886e9a9a06ebb58b42ade5
jkimoto28 <jkimoto@hawk.iit.edu>
2025-04-03 23:02:46 -0500

    Create script.js
    added in code to make sure the html page is loaded before any javascript code runs, selected the form element, and wrote code to prevent default submission

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("signup-form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+    });
+});
